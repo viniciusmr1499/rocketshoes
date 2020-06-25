6a6f0e77430f94ffe33ef9a7e9f4f905eaad519f
viniciusmr1499 <viniciusmr1499@gmail.com>
2020-06-24 22:38:38 -0300

    estrutura basica

---
+++ src/styles/global.js
@@ -0,0 +1,31 @@
+import createGlobalStyle from 'styled-components';
+
+export const globalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+    * {
+        outline: none;
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body, html {
+        background: #191919;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, button {
+        font: 14px 'Roboto', sans-serif;
+    }
+
+    #root {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 0 20px 50px;
+    }
+
+    button {
+        cursor: pointer;  
+    }
+`;
