bc8cf8d1238b35c3523d763c1d81b2dfbd7471f8
Marcos Vinicius <viniciusmr1499@gmail.com>
2020-08-17 09:04:31 -0300

    server.json configurado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,118 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 
 
 export default function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        api.get('products').then(response => {
+            setProducts(response.data);
+        }).catch(err => {
+            console.error(err);
+        });
+        
+    }, []);
+    
     return (
         <ProductList>
-            <li>
-                <img
-                    src="https://img.irroba.com.br/fit-in/600x600/filters:fill(fff):quality(90)/lojadoct/catalog//loja/calcados/masculinos/sapatos/sapato-masculino-branco-1919-2.jpg"
-                    alt="Tênis"
+            {products.map(product => (
+                <li key={product.id}>
+                    <img src={product.image} alt={product.title}/>
+                    <strong>{product.title}</strong>
+                    <span>{product.price}</span>
 
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
+                    <button type="button">
+                        <div>
+                            <MdAddShoppingCart size={16} colo="#fff" />
+                        </div>
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} colo="#fff" />
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-                    src="https://lojarafarillo.vteximg.com.br/arquivos/ids/157780-574-574/5901-01--1-.jpg?v=636341567311100000"
-                    alt="Tênis"
-
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} colo="#fff" />
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-                    src="https://img.irroba.com.br/fit-in/600x600/filters:fill(fff):quality(90)/lojadoct/catalog//loja/calcados/masculinos/sapatos/sapato-masculino-branco-1919-2.jpg"
-                    alt="Tênis"
-
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} colo="#fff" />
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-                    src="https://lojarafarillo.vteximg.com.br/arquivos/ids/157780-574-574/5901-01--1-.jpg?v=636341567311100000"
-                    alt="Tênis"
-
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} colo="#fff" />
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-                    src="https://img.irroba.com.br/fit-in/600x600/filters:fill(fff):quality(90)/lojadoct/catalog//loja/calcados/masculinos/sapatos/sapato-masculino-branco-1919-2.jpg"
-                    alt="Tênis"
-
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} colo="#fff" />
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-                    src="https://lojarafarillo.vteximg.com.br/arquivos/ids/157780-574-574/5901-01--1-.jpg?v=636341567311100000"
-                    alt="Tênis"
-
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} colo="#fff" />
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
+                        <span>ADICIONAR AO CARRINHO</span>
+                    </button>
+                </li>
+            ))}
         </ProductList>
     );
 }
